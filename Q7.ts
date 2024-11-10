8703ee313720aecd2e4d6477456d3cd45b7acd10
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:07:45 +0500

    Add files via upload

---
+++ Q7.ts
@@ -0,0 +1,25 @@
+//7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+function add(num1: number,num2: number){
+    console.log(num1+num2);
+}
+
+add(5,3)
+
+function sub(num1: number,num2: number){
+    console.log(num1-num2);
+}
+
+sub(10,2)
+
+function multi(num1: number,num2: number){
+    console.log(num1*num2);
+}
+
+multi(4,2)
+
+function division(num1: number,num2: number){
+    console.log(num1/num2);
+}
+
+division(64,8)
